ae77b3f3c62942b98ad79bb90ece29b6d63607a5
yjk395 <68368755+yjk395@users.noreply.github.com>
2022-03-24 12:47:42 +0900

    [브루트 포스] 3월 24일 - ing

--- 03월 18일 - 브루트 포스/1759.cpp
+++ 03월 18일 - 브루트 포스/1759.cpp
@@ -13,9 +13,9 @@ vector<char> vowel;
 //자음, 모음에서 선택된 수를 string에 추가
 void makePw(string &str, vector<bool> &temp, vector<char> &alp) {
     for (int i = 0; i < temp.size(); i++) {
-        if(temp[i]){
+        if (temp[i]) {
             str.push_back(alp[i]);
-//            cout<<alp[i]<<' ';
+            cout<<alp[i]<<' '; //디버깅
         }
     }
 }
@@ -23,15 +23,25 @@ void makePw(string &str, vector<bool> &temp, vector<char> &alp) {
 //가능성 있는 암호를 모두 저장한 벡터를 구하는 함수
 vector<string> findPossiblePw(int l) {
     //조합 구하기 위한 임시 벡터
-    vector<bool> c_temp(consonant.size(), false);
+    vector<bool> c_temp(consonant.size(), true);
     vector<bool> v_temp(vowel.size(), false);
 
     string str;
     vector<string> answer;
 
-    for (int i = 1; i <= l - 2; i++) { //모음을 i개 선택
-        fill(v_temp.begin(), v_temp.begin() + i - 1, true); //임시벡터 수정
-        fill(c_temp.begin(), c_temp.begin() + l - i - 1, true);
+    int v_size = vowel.size(); //l-2가 가능한 모음 개수보다 큰 경우 있음 나머지 개수만큼은 비어버림. 둘 중 작은 것으로 고름
+
+    //l == c이면 왜 무한루프?
+    for (int i = 1; i <= min(v_size, l - 2); i++) { //모음을 i개 선택
+//        fill(v_temp.begin(), v_temp.begin() + i, true); //임시벡터 수정
+        fill(c_temp.begin(), c_temp.end() - (l - i), false); //모음은 선택 개수 점점 줄어듦->false를 증가시킴
+        v_temp[i-1] = true; //임시 벡터 수정
+
+        //디버깅
+        for(bool b:c_temp){
+            cout<<b<<' ';
+        }
+        cout<<'\n';
 
         do {
             makePw(str, v_temp, vowel); //선택된 모음을 string에 추가
@@ -39,16 +49,17 @@ vector<string> findPossiblePw(int l) {
             //자음 조합 구하기
             do {
                 makePw(str, c_temp, consonant); //선택된 자음을 string에 추가
-                sort(str.begin(), str.end());
-                answer.push_back(str);
-                str = str_tmp;
-            } while (prev_permutation(c_temp.begin(), c_temp.end()));
+                sort(str.begin(), str.end()); //암호 사전식 정렬
+                answer.push_back(str); //결과 벡터에 삽입
+                str = str_tmp; //반복 위해 초기화
+                cout<<'\n'; //디버깅
+            } while (next_permutation(c_temp.begin(), c_temp.end()));
+            str = ""; //반복 위해 초기화
         } while (prev_permutation(v_temp.begin(), v_temp.end()));
     }
 
-    sort(answer.begin(), answer.end());
+    sort(answer.begin(), answer.end()); //결과 벡터 사전식 정렬
     return answer;
-
 }
 
 //l개 중 1개 이상의 모음 선택, 나머진 자음 선택하고 알파벳을 사전순 배치
