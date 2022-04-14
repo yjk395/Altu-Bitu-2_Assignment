15570b7cc29cb400b0ec7f863a3e3f4097b4a85a
yjk395 <68368755+yjk395@users.noreply.github.com>
2022-04-14 17:54:21 +0900

    Update 19637.cpp

--- 04월 08일 - 이분 탐색/19637.cpp
+++ 04월 08일 - 이분 탐색/19637.cpp
@@ -6,21 +6,21 @@
 
 using namespace std;
 
-const int MAX = 1e9;
-
 int main() {
     int n, m; //칭호, 캐릭터의 개수
     cin >> n >> m;
     vector<string> title(n);
-    vector<int> bound(n);
+    vector<int> bound(n + 1);
+    bound[0] = -1; //전투력은 0부터, 더 작은 값 첫 번째 인덱스에 저장
     vector<int> character(m);
     for (int i = 0; i < n; i++) { //칭호의 이름, 전투력 상한값 입력
-        cin >> title[i] >> bound[i];
+        cin >> title[i] >> bound[i + 1];
     }
-    bound.insert(bound.begin(), -1);
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) { //캐릭터의 전투력 입력
         cin >> character[i];
     }
+
+    sort(character.begin(), character.end()); //오름차순 정렬
 
     //출력
     for (int i = 0; i < n; i++) {
@@ -31,7 +31,6 @@ int main() {
             cout << title[i] << '\n';
         }
     }
-
 
     return 0;
 }
