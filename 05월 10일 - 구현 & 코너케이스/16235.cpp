f1172c6932cc6ca1b97b603511ba44aee78a23e7
yjk395 <68368755+yjk395@users.noreply.github.com>
2022-05-16 16:54:27 +0900

    Update 16235.cpp

--- 05월 10일 - 구현 & 코너케이스/16235.cpp
+++ 05월 10일 - 구현 & 코너케이스/16235.cpp
@@ -1,17 +1,106 @@
 //아기 상어
-/*
- * 상어 크기보다 작은 물고기들 중 가까운 애 찾아서 거기로 이동
- * 이동할 때 큰 물고기는 통과불가
- * 크기만큼 물고기 먹으면 크기 ++
- */
 
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <set>
 
 using namespace std;
 
-int main(){
+typedef pair<int, int> ci;
+
+//아기 상어의 좌표 반환
+ci findShark(int n, vector<vector<int>> &space) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (space[i][j] == 9) return ci(i, j);
+        }
+    }
+}
+
+//너비 우선 탐색, 이동할 곳 찾아 이동 후 추가되는 시간 반환
+int bfs(int n, vector<vector<int>> &space, int size) {
+    // 상, 하, 좌, 우
+    int dr[] = {-1, +1, 0, 0};
+    int dc[] = {0, 0, -1, +1};
+
+    queue<ci> que;  //탐색을 하기 위해 사용할 큐
+    int dist = 0; //먹이 물고기까지의 거리(=걸리는 시간)
+    vector<vector<int>> distance(n, vector<int>(n, 0)); //아기 상어로부터 해당 칸까지의 거리 기록
+    set<ci> feed_candidate; //먹이 후보 찾아서 저장, 자동 정렬
+
+    //아기 상어의 좌표 큐에 삽입
+    que.push(findShark(n, space));
+    distance[que.front().first][que.front().second] = dist; //상어 자신의 거리는 0
+    space[que.front().first][que.front().second] = 0; //상어 떠날 자리는 빈자리가 됨
+
+    while (!que.empty()) {
+        auto[r, c] = que.front(); //탐색할 칸 좌표
+        que.pop();
+
+        if (space[r][c] > 0 && space[r][c] < size) { //상어보다 작은 물고기이면
+            if (!dist) { //가장 가까운 물고기 처음 등장
+                dist = distance[r][c];
+                feed_candidate.insert({r, c});
+            } else if (distance[r][c] == dist) { //가장 가까운 물고기 여러 마리
+                feed_candidate.insert({r, c});
+            }
+        }
+
+        //상하좌우 네 방향 탐색
+        for (int i = 0; i < 4; i++) {
+            int new_r = r + dr[i];
+            int new_c = c + dc[i];
+
+            //범위체크 및 통과할 수 있는지 확인, 방문여부도 확인
+            if (new_r >= 0 && new_r < n && new_c >= 0 && new_c < n && space[new_r][new_c] <= size &&
+                !distance[new_r][new_c]) {
+                que.push({new_r, new_c}); //큐에 삽입
+                distance[new_r][new_c] = distance[r][c] + 1;
+            }
+        }
+    }
+
+    //먹을 수 있는 물고기가 없다면 -1 반환
+    if (!dist) return -1;
+
+    auto it = feed_candidate.begin(); //먹이 후보 중 가장 위, 가장 왼쪽 물고기
+    space[it->first][it->second] = 9; //상어 이동
+
+    return dist; //이동에 걸린 시간 반환
+}
+
+int main() {
     int n; //공간의 크기
-    cin>>n;
+    cin >> n;
+
+    vector<vector<int>> space(n, vector<int>(n, 0)); //공간 상태
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> space[i][j]; //공간의 상태 입력
+        }
+    }
+
+    int time = 0, size = 2, feed_cnt = 0; //걸린 시간, 상어의 크기, 물고기 먹은 횟수
+
+    //연산
+    while (true) {
+        int dt = bfs(n, space, size); //탐색
+        if (dt == -1) break; //먹을 수 있는 물고기 없으면 종료
+
+        feed_cnt++; //물고기 먹은 횟수 카운트
+        if (feed_cnt >= size) { //자신의 크기와 같은 수의 물고기 먹으면
+            size++; //크기 1 증가
+            feed_cnt = 0; //물고기 먹은 횟수 초기화
+        }
+
+        time += dt; //시간 카운트
+    }
+
+    //출력
+    cout << time;
 
     return 0;
 }
