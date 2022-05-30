2b7be93ffa2fe1c15d4a57662366dbdd3e289817
yjk395 <68368755+yjk395@users.noreply.github.com>
2022-05-30 15:51:35 +0900

    Update 1043.cpp

--- 05월 24일 - 유니온 파인드/1043.cpp
+++ 05월 24일 - 유니온 파인드/1043.cpp
@@ -31,14 +31,31 @@ void unionInput(int a, int b, int i) {
     }
 }
 
+//a를 루트로 b를 union
+void unionInput2(int a, int b, int i) {
+    if (truth[b]) truth[a] = true;
+    parent[i][b] = a;
+}
+
+bool doesPartyKnowTruth(int n, int m, int party_num) { //파티 번호 주어짐
+    for (int i = 1; i < n + 1; i++) { //전체 사람
+        if (parent[party_num][i] < 0) continue; //파티에 오지 않았으면 건너뜀
+        if(truth[i]) return true;
+        for (int j = 0; j < m; j++) { //모든 파티에 대해
+            if (truth[parent[j][i]]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main() {
     int n, m, p, a, b; //사람의 수, 파티의 수, 각 사람의 수, 사람 번호
     cin >> n >> m >> p;
 
     parent.assign(m, vector<int>(n + 1, -1));
     truth.assign(n + 1, false);
-
-    vector<int> party(0);
 
     //진실을 아는 사람 입력
     for (int i = 0; i < p; i++) {
@@ -49,20 +66,16 @@ int main() {
     for (int i = 0; i < m; i++) {
         cin >> p; //파티에 오는 사람 수
         cin >> a;
-        party.push_back(a);
+        parent[i][a] = a;
         for (int j = 0; j < p - 1; j++) {
             cin >> b;
-            unionInput(b, a, i); //파티에 오는 사람을 같은 집합으로
+            unionInput2(a, b, i); //파티에 오는 사람을 같은 집합으로
         }
     }
 
     int cnt = 0;
-    for (int i = 0; i < m; i++) {
-        bool know_truth = false;
-        for (int j = 0; j < m; j++) {
-            if (truth[findParent(party[i], j)]) know_truth = true;
-        }
-        if (!know_truth) cnt++;
+    for (int i = 0; i < m; i++) { //모든 파티
+        if (!doesPartyKnowTruth(n, m, i)) cnt++;
     }
 
     cout << cnt;
