4a6427ccfceb145957253c7253ba37101ef464f1
yjk395 <68368755+yjk395@users.noreply.github.com>
2022-05-30 14:55:50 +0900

    Update 호텔 방 배정.cpp

--- 05월 24일 - 유니온 파인드/호텔 방 배정.cpp
+++ 05월 24일 - 유니온 파인드/호텔 방 배정.cpp
@@ -7,21 +7,22 @@
 using namespace std;
 
 vector<long long> room_number;
-unordered_map<long long, long long> sold_room; //아 parent배열에서 값을 다음으로 나올 빈 방으로
+unordered_map<long long, long long> sold_room; //parent배열에서 값을 다음으로 나올 빈 방으로
 
+//배정할 방을 반환
 long long findNextRoom(long long n) {
     if (sold_room[n] == 0) { //방이 비어있으면
-        sold_room[n] = n + 1; //value는 다음 방을 가리킴
+        sold_room[n] = n + 1; //맵의 value는 다음 방을 가리킴
         return n;
     }
-    return sold_room[n] = findNextRoom(sold_room[n]); //다음 빈 방 찾아서 업데이트 후 반환
+    return sold_room[n] = findNextRoom(sold_room[n]); //다음 빈 방으로 재귀호출하여 value 업데이트 후 반환
 }
 
 vector<long long> solution(long long k, vector<long long> room_number) {
     vector<long long> answer;
 
     for (int i = 0; i < room_number.size(); i++) {
-        answer.push_back(findNextRoom(room_number[i]));
+        answer.push_back(findNextRoom(room_number[i])); //방 배정
     }
 
     return answer;
@@ -30,7 +31,7 @@ vector<long long> solution(long long k, vector<long long> room_number) {
 int main() {
     long long k; //전체 방 개수
     cin >> k;
-    int num; //손님 수
+    int num; //손님이 제출한 방
 
     while (cin >> num) {
         room_number.push_back(num);
